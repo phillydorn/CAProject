4cfdeaafca6d962e0c45066b53e971b3adcc39b5
Phil Dornfeld <phillydorn75@gmail.com>
2015-10-27 23:18:52 -0700

    process env sport radar

--- server/helpers/schoolFunctions.js
+++ server/helpers/schoolFunctions.js
@@ -3,13 +3,16 @@ var request = require('request');
 
 if (!process.env.CLIENT_ID) {
   var keys = require('../keys.js');
+  var sportRadarKey = keys.sportradarKey;
+  } else {
+    var sportRadarKey = process.env.CLIENT_ID;
   }
 
 module.exports = {
   fetchAllSchools: function(req, res) {
     request({
       // url: 'http://api.sportradar.us/ncaamb-t3/tournaments/83c03d12-e03b-4f71-846c-5d42ba90eeb1/schedule.json?api_key='+keys.sportradarKey,
-      url: 'http://api.sportradar.us/ncaamb-t3/tournaments/83c03d12-e03b-4f71-846c-5d42ba90eeb1/summary.json?api_key='+keys.sportradarKey,
+      url: 'http://api.sportradar.us/ncaamb-t3/tournaments/83c03d12-e03b-4f71-846c-5d42ba90eeb1/summary.json?api_key='+sportRadarKey,
     },
       function(err, resp, body) {
         var brackets = JSON.parse(body).brackets;
